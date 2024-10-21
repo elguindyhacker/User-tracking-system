1432493d5cdb2624259ae89eead39c41f546aa5e
Abdulrahman Elguindy <abdulrahmanelguindy@gmail.com>
2024-10-21 17:50:41 +0300

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,23 +1,23 @@
-let users = JSON.parse(localStorage.getItem('users')) || {};
-console.log(users);
-
-document.querySelector('form')
-    .addEventListener('submit', function(event) {
-        event.preventDefault();
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
-        createAccount(email, password);
-    });
+const users = JSON.parse(localStorage.getItem('users'));
 
 function createAccount(email, password) {
     if (users[email]) {
         console.log("This one used already, Pick a different email.");
-        alert("This email is already used, Pick a different email.");
+        alert("This one used already, Pick a different email.");
+        return;
     } else {
         users[email] = {password};
         localStorage.setItem('users', JSON.stringify(users))
-        console.log(users);
+        localStorage.setItem("signedUpEmail", email);
         console.log("Account created");
         alert("Account created");
+        return true;
+
     }
 }
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = docuemnt.getElementById('password').value;
+createAccount(email, password);
+});
