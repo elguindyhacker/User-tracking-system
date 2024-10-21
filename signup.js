42178dacd0b3379aba2cd121f45cb3e1e3c44d0a
MohamedElsaeed424 <mohamed.smenshawy@gmail.com>
2024-10-21 14:02:24 +0300

    project evaluated

--- signup.js
+++ signup.js
@@ -1,20 +1,23 @@
-const users = JSON.parse(localStorage.getItem('users'));
+let users = JSON.parse(localStorage.getItem('users')) || {};
+console.log(users);
+
+document.querySelector('form')
+    .addEventListener('submit', function(event) {
+        event.preventDefault();
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        createAccount(email, password);
+    });
 
 function createAccount(email, password) {
     if (users[email]) {
         console.log("This one used already, Pick a different email.");
-        return;
+        alert("This email is already used, Pick a different email.");
     } else {
         users[email] = {password};
         localStorage.setItem('users', JSON.stringify(users))
+        console.log(users);
         console.log("Account created");
-        return true;
-
+        alert("Account created");
     }
 }
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = docuemnt.getElementById('password').value;
-createAccount(email, password);
-});
